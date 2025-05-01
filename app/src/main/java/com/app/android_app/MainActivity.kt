b4f0fe01aa4887f69cfba468940dad36c2a6b604
Judith Lecoq <judith.lecoq@student.junia.com>
2025-05-01 19:54:54 +0200

    fix: composable version & upload

--- app/src/main/java/com/app/android_app/MainActivity.kt
+++ app/src/main/java/com/app/android_app/MainActivity.kt
@@ -3,6 +3,18 @@ package com.app.android_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.*
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.app.android_app.ui.theme.*
 import com.app.android_app.ui.upload.UploadScreen
 
@@ -10,7 +22,74 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            AndroidappTheme {
+                val nav = rememberNavController()
+                AppNavigation(nav)
+            }
+        }
+    }
+}
+
+@Composable
+fun AppNavigation(nav: NavHostController){
+    NavHost(nav, startDestination = "main"){
+        composable("main") {
+            MainScreen(nav)
+        }
+        composable("upload") {
             UploadScreen()
+        }
+        composable("download") {
+            DownloadScreen()
+        }
+        composable("list"){
+            ListScreen()
+        }
+    }
+}
+
+@Composable
+fun ListScreen() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun DownloadScreen() {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun MainScreen(nav: NavHostController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = { nav.navigate("upload") },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Send a file")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { nav.navigate("download") },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Receive a file")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { nav.navigate("list") },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("List all files")
         }
     }
 }
